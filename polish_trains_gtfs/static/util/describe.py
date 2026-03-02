2c415c2c2138d68ec1ee610302d4fdc482b6c3c8
Mikołaj Kuranowski <mkuranowski@gmail.com>
2026-03-02 11:21:56 +0100

    Static: add route name to trip description

--- polish_trains_gtfs/static/util/describe.py
+++ polish_trains_gtfs/static/util/describe.py
@@ -7,11 +7,16 @@ from impuls import DBConnection
 
 
 def trip(db: DBConnection, trip_id: str) -> str:
-    with db.raw_execute("SELECT short_name FROM trips WHERE trip_id = ?", (trip_id,)) as q:
+    with db.raw_execute(
+        "SELECT trips.short_name, routes.short_name "
+        "FROM trips LEFT JOIN routes USING (route_id) "
+        "WHERE trip_id = ?",
+        (trip_id,),
+    ) as q:
         row = q.one()
         if row is None:
             return f"trip {trip_id!r}"  # trip does not exists, short-circuit
-        short_name = cast(str, row[0])
+        trip_name, route_name = cast(tuple[str, str], row)
 
     with db.raw_execute(
         "SELECT name FROM stop_times LEFT JOIN stops USING (stop_id) "
@@ -30,5 +35,5 @@ def trip(db: DBConnection, trip_id: str) -> str:
         last_stop = cast(str, row[0]) if row else ""
 
     if first_stop:
-        return f"trip {trip_id!r} ({short_name}, {first_stop} -> {last_stop})"
-    return f"trip {trip_id!r} ({short_name})"
+        return f"trip {trip_id!r} ({route_name} {trip_name}, {first_stop} -> {last_stop})"
+    return f"trip {trip_id!r} ({route_name} {trip_name})"
